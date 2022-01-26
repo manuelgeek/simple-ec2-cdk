37129dbe0382d70b27058e9e48cda3551f4a211e
Magak Emmanuel <emashmagak@gmail.com>
2022-01-26 15:30:03 +0300

    added ec2 definition

--- bin/simple-ec2.ts
+++ bin/simple-ec2.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
+import * as cdk from '@aws-cdk/core';
 import { SimpleEc2Stack } from '../lib/simple-ec2-stack';
 
 const app = new cdk.App();
